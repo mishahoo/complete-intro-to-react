d2333b069329e39bd6c978ad77c0b82762642247
Misha Hoo <hoomisha@gmail.com>
2018-01-02 18:50:04 -0600

    Add a search input with onChange handler for Search component

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ShowCard from './ShowCard';
 import preload from '../data.json';
 
-const Search = () => (
-  <div className="search">
-    <div>{preload.shows.map(show => <ShowCard show={show} key={show.imdbID} />)}</div>
-  </div>
-);
+class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: ''
+    };
+    this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
+  }
+
+  handleSearchTermChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>svideo</h1>
+          <input
+            onChange={this.handleSearchTermChange}
+            value={this.state.searchTerm}
+            type="text"
+            placeholder="Search"
+          />
+        </header>
+        <div>{preload.shows.map(show => <ShowCard show={show} key={show.imdbID} />)}</div>
+      </div>
+    );
+  }
+}
 
 export default Search;
