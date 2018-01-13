d9172fe4a149d2b04a19c39d807f45d13cfe530a
Misha Hoo <hoomisha@gmail.com>
2018-01-12 18:16:40 -0600

    Refactor redux for flow.

--- js/actionCreators.js
+++ js/actionCreators.js
@@ -1,5 +1,7 @@
+// @flow
+
 import { SET_SEARCH_TERM } from './actions';
 
-export function setSearchTerm(SearchTerm) {
+export function setSearchTerm(SearchTerm: string) {
   return { type: SET_SEARCH_TERM, payload: SearchTerm };
 }
