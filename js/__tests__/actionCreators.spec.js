9942f43de911adec60eb54ecd490e7fdd34e51d6
Misha Hoo <hoomisha@gmail.com>
2018-01-13 13:19:35 -0600

    Test Thunk getAPIDetails.

--- js/__tests__/actionCreators.spec.js
+++ js/__tests__/actionCreators.spec.js
@@ -1,22 +1,43 @@
 // @flow
 
-import { setSearchTerm, addAPIData } from '../actionCreators';
+import moxios from 'moxios';
+import { setSearchTerm, addAPIData, getAPIDetails } from '../actionCreators';
+
+const billions = {
+  title: 'Billions',
+  year: '2016–',
+  description:
+    'U.S. Attorney Chuck Rhoades goes after hedge fund king, Bobby "Axe" Axelrod in a battle between two powerful New York figures.',
+  poster: 'b.jpg',
+  imdbID: 'tt4270492',
+  trailer: '_raEUMLL-ZI',
+  rating: '8.6'
+};
 
 test('setSearchTerm', () => {
   expect(setSearchTerm('New York')).toMatchSnapshot();
 });
 
 test('addAPIData', () => {
-  expect(
-    addAPIData({
-      title: 'Billions',
-      year: '2016–',
-      description:
-        'U.S. Attorney Chuck Rhoades goes after hedge fund king, Bobby "Axe" Axelrod in a battle between two powerful New York figures.',
-      poster: 'b.jpg',
-      imdbID: 'tt4270492',
-      trailer: '_raEUMLL-ZI',
-      rating: '8.6'
-    })
-  ).toMatchSnapshot();
+  expect(addAPIData(billions)).toMatchSnapshot();
+});
+
+test('getAPIDetails', (done: Function) => {
+  const dispatchMock = jest.fn();
+  moxios.withMock(() => {
+    getAPIDetails(billions.imdbID)(dispatchMock);
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request
+        .respondWith({
+          status: 200,
+          response: billions
+        })
+        .then(() => {
+          expect(request.url).toEqual(`http://localhost:3000/${billions.imdbID}`);
+          expect(dispatchMock).toBeCalledWith(addAPIData(billions));
+          done();
+        });
+    });
+  });
 });
